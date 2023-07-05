31d554c84c294ccc293899a6d16074e2bbd6a9f7
luis <lmstive@gmail.com>
2023-07-05 15:05:59 -0300

    Aula Data e Hora

---
+++ src/app/Programa1.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Programa1 {
+
+	public static void main(String[] args) {
+		// https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm");
+		
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now();
+		
+		LocalDate d04 = LocalDate.parse("2023-07-05");
+		LocalDateTime d05 = LocalDateTime.parse("2023-07-05T13:59:23");
+		Instant d06 = Instant.parse("2023-07-05T13:59:23Z");
+		Instant d07 = Instant.parse("2023-07-05T13:59:23-03:00");
+		
+		LocalDate d08 = LocalDate.parse("05/07/2023", fmt1);
+		LocalDateTime d09 = LocalDateTime.parse("05/07/2023 01:30", fmt2);
+		
+		LocalDate d10 = LocalDate.of(2023, 7, 20);
+		LocalDateTime d11 = LocalDateTime.of(2023, 7, 20, 1, 30);
+ 		
+		System.out.println("d01 = " + d01.toString());
+		System.out.println("d02 = " + d02);
+		System.out.println("d03 = " + d03);
+		System.out.println("d04 = " + d04);
+		System.out.println("d05 = " + d05);
+		System.out.println("d06 = " + d06);
+		System.out.println("d07 = " + d07);
+		System.out.println("d08 = " + d08);
+		System.out.println("d09 = " + d09);
+		System.out.println("d10 = " + d10);
+		System.out.println("d11 = " + d11);
+	}
+
+}
